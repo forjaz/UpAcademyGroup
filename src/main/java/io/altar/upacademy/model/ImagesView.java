69b13a1b26af00741588e22396da9b8d2f5d3c1b
aluno tarde <alunotarde@Altars-Mini-5.lan>
2017-06-05 18:07:26 +0100

    Default Page

---
+++ src/main/java/io/altar/upacademy/model/ImagesView.java
@@ -0,0 +1,26 @@
+package io.altar.upacademy.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ImagesView {
+	
+	private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 12; i++) {
+            images.add("Imagem" + i);
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+	
+
+}
