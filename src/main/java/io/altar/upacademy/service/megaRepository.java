775e74ab14641d3248336eb731e8df1a12d8ef5d
aluno tarde <alunotarde@altars-Mini-45.lan>
2017-06-12 18:41:55 +0100

    no message

--- src/main/java/io/altar/upacademy/service/megaRepository.java
+++ src/main/java/io/altar/upacademy/service/megaRepository.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 
 import io.altar.upacademy.model.Ingrediente;
 import io.altar.upacademy.model.Receita;
+import io.altar.upacademy.model.Receita_Ingrediente;
 
 
 
@@ -63,12 +64,18 @@ public class megaRepository extends EntityService implements Serializable{
 	public String megaInit(){
 		initReceitas();
 		initIng1();
+		initIng2();
+		initIng3();
+		initIng_R1();
+		initIng_R2();
+		initIng_R3();
 		return "receitas";
 	}
 	
 	public void initIng1() {
 		Ingrediente ing1 = new Ingrediente();
-		ing1.setNome("bróculos cozidos");
+		ing1.setNome("bróculos");
+		ing1.setModoPreparacao("cozido");
 		ing1.setCalorias(25);
 		ing1.setGorduras(0.5);
 		ing1.setHidratos(4.4);
@@ -76,7 +83,8 @@ public class megaRepository extends EntityService implements Serializable{
 		em.persist(ing1);
 		
 		Ingrediente ing2 = new Ingrediente();
-		ing2.setNome("esparguete cozido");
+		ing2.setNome("esparguete");
+		ing2.setModoPreparacao("cozido");
 		ing2.setCalorias(101);
 		ing2.setGorduras(0.6);
 		ing2.setHidratos(19.9);
@@ -84,7 +92,8 @@ public class megaRepository extends EntityService implements Serializable{
 		em.persist(ing2);
 		
 		Ingrediente ing3 = new Ingrediente();
-		ing3.setNome("cebola frita");
+		ing3.setNome("cebola");
+		ing3.setModoPreparacao("frito");
 		ing3.setCalorias(132);
 		ing3.setGorduras(11.2);
 		ing3.setHidratos(6.2);
@@ -93,6 +102,7 @@ public class megaRepository extends EntityService implements Serializable{
 		
 		Ingrediente ing4 = new Ingrediente();
 		ing4.setNome("alho");
+		ing4.setModoPreparacao("frito");
 		ing4.setCalorias(67);
 		ing4.setGorduras(0.6);
 		ing4.setHidratos(11.3);
@@ -100,7 +110,8 @@ public class megaRepository extends EntityService implements Serializable{
 		em.persist(ing4);
 		
 		Ingrediente ing5 = new Ingrediente();
-		ing5.setNome("aipo ");
+		ing5.setNome("aipo");
+		ing5.setModoPreparacao("frito");
 		ing5.setCalorias(11);
 		ing5.setGorduras(0.1);
 		ing5.setHidratos(1.5);
@@ -115,6 +126,289 @@ public class megaRepository extends EntityService implements Serializable{
 		ing6.setProteina(0);
 		em.persist(ing6);
 		
-	}
+		Ingrediente ing7 = new Ingrediente();
+		ing7.setNome("tomate");
+		ing7.setModoPreparacao("frito");
+		ing7.setCalorias(309);
+		ing7.setGorduras(3.3);
+		ing7.setHidratos(9.0);
+		ing7.setProteina(1.4);
+		em.persist(ing7);
 		
+		Ingrediente ing8 = new Ingrediente();
+		ing8.setNome("vinho branco");
+		ing8.setCalorias(302);
+		ing8.setGorduras(0);
+		ing8.setHidratos(1.2);
+		ing8.setProteina(0.1);
+		em.persist(ing8);
+		
+		Ingrediente ing9 = new Ingrediente();
+		ing9.setNome("frango");
+		ing9.setModoPreparacao("cozido");
+		ing9.setCalorias(204);
+		ing9.setGorduras(11.1);
+		ing9.setHidratos(0);
+		ing9.setProteina(26.1);
+		em.persist(ing9);
+		
+		Ingrediente ing10 = new Ingrediente();
+		ing10.setNome("azeitonas pretas");
+		ing10.setCalorias(137);
+		ing10.setGorduras(14.2);
+		ing10.setHidratos(4.1);
+		ing10.setProteina(0.9);
+		em.persist(ing10);
+	}
+	
+	public void initIng2() {
+		Ingrediente ing1 = new Ingrediente();
+		ing1.setNome("funcho Bolbo");
+		ing1.setModoPreparacao("frito");
+		ing1.setCalorias(40);
+		ing1.setGorduras(0);
+		ing1.setHidratos(7);
+		ing1.setProteina(1);
+		em.persist(ing1);
+		
+		Ingrediente ing2 = new Ingrediente();
+		ing2.setNome("cenoura");
+		ing2.setModoPreparacao("frita");
+		ing2.setCalorias(60);
+		ing2.setGorduras(4.79);
+		ing2.setHidratos(8);
+		ing2.setProteina(0.74);
+		em.persist(ing2);
+		
+		Ingrediente ing3 = new Ingrediente();
+		ing3.setNome("ovos de galinha");
+		ing3.setModoPreparacao("cozido");
+		ing3.setCalorias(149);
+		ing3.setGorduras(10.8);
+		ing3.setHidratos(0);
+		ing3.setProteina(13);
+		em.persist(ing3);
+		
+		Ingrediente ing4 = new Ingrediente();
+		ing4.setNome("cebolinho");
+		ing4.setModoPreparacao("cru");
+		ing4.setCalorias(0);
+		ing4.setGorduras(0);
+		ing4.setHidratos(0);
+		ing4.setProteina(0);
+		em.persist(ing4);
+		
+		Ingrediente ing5 = new Ingrediente();
+		ing5.setNome("açucar");
+		ing5.setCalorias(387);
+		ing5.setGorduras(0);
+		ing5.setHidratos(100);
+		ing5.setProteina(0);
+		em.persist(ing5);
+		
+	}
+	
+	public void initIng3() {
+		Ingrediente ing1 = new Ingrediente();
+		ing1.setNome("borrgo (pá)");
+		ing1.setModoPreparacao("frito");
+		ing1.setCalorias(166);
+		ing1.setGorduras(7.6);
+		ing1.setHidratos(0);
+		ing1.setProteina(24.5);
+		em.persist(ing1);
+		
+		Ingrediente ing2 = new Ingrediente();
+		ing2.setNome("vinagre de sidra");
+		ing2.setCalorias(210);
+		ing2.setGorduras(0);
+		ing2.setHidratos(9.5);
+		ing2.setProteina(0);
+		em.persist(ing2);
+		
+		Ingrediente ing3 = new Ingrediente();
+		ing3.setNome("batata");
+		ing3.setModoPreparacao("assado");
+		ing3.setCalorias(152);
+		ing3.setGorduras(4.8);
+		ing3.setHidratos(23.7);
+		ing3.setProteina(3);
+		em.persist(ing3);
+		
+		Ingrediente ing4 = new Ingrediente();
+		ing4.setNome("alho francês");
+		ing4.setCalorias(0);
+		ing4.setGorduras(0.0);
+		ing4.setHidratos(0);
+		ing4.setProteina(0);
+		em.persist(ing4);
+		
+		Ingrediente ing5 = new Ingrediente();
+		ing5.setNome("maçã reineta");
+		ing5.setModoPreparacao("frito");
+		ing5.setCalorias(66);
+		ing5.setGorduras(0.5);
+		ing5.setHidratos(15.9);
+		ing5.setProteina(0.3);
+		em.persist(ing5);
+		
+		Ingrediente ing6 = new Ingrediente();
+		ing6.setNome("coentros");
+		ing6.setCalorias(66);
+		ing6.setGorduras(0.5);
+		ing6.setHidratos(15.9);
+		ing6.setProteina(0.3);
+		em.persist(ing6);
+	}
+	
+	public void initIng_R1() {
+		Receita_Ingrediente ir1 = new Receita_Ingrediente();
+		ir1.setReceita(em.find(Receita.class, 1L));
+		ir1.setIngrediente(em.find(Ingrediente.class, 1L));
+		ir1.setQuantidade(200);
+		em.persist(ir1);
+		
+		Receita_Ingrediente ir2 = new Receita_Ingrediente();
+		ir2.setReceita(em.find(Receita.class, 1L));
+		ir2.setIngrediente(em.find(Ingrediente.class, 2L));
+		ir2.setQuantidade(200);
+		em.persist(ir2);
+		
+		Receita_Ingrediente ir3 = new Receita_Ingrediente();
+		ir3.setReceita(em.find(Receita.class, 1L));
+		ir3.setIngrediente(em.find(Ingrediente.class, 3L));
+		ir3.setQuantidade(200);
+		em.persist(ir3);
+		
+		Receita_Ingrediente ir4 = new Receita_Ingrediente();
+		ir4.setReceita(em.find(Receita.class, 1L));
+		ir4.setIngrediente(em.find(Ingrediente.class, 4L));
+		ir4.setQuantidade(200);
+		em.persist(ir4);
+		
+		Receita_Ingrediente ir5 = new Receita_Ingrediente();
+		ir5.setReceita(em.find(Receita.class, 1L));
+		ir5.setIngrediente(em.find(Ingrediente.class, 5L));
+		ir5.setQuantidade(200);
+		em.persist(ir5);
+		
+		Receita_Ingrediente ir6 = new Receita_Ingrediente();
+		ir6.setReceita(em.find(Receita.class, 1L));
+		ir6.setIngrediente(em.find(Ingrediente.class, 6L));
+		ir6.setQuantidade(200);
+		em.persist(ir6);
+		
+		Receita_Ingrediente ir7 = new Receita_Ingrediente();
+		ir7.setReceita(em.find(Receita.class, 1L));
+		ir7.setIngrediente(em.find(Ingrediente.class, 7L));
+		ir7.setQuantidade(200);
+		em.persist(ir7);
+		
+		Receita_Ingrediente ir8 = new Receita_Ingrediente();
+		ir8.setReceita(em.find(Receita.class, 1L));
+		ir8.setIngrediente(em.find(Ingrediente.class, 8L));
+		ir8.setQuantidade(200);
+		em.persist(ir8);
+		
+		Receita_Ingrediente ir9 = new Receita_Ingrediente();
+		ir9.setReceita(em.find(Receita.class, 1L));
+		ir9.setIngrediente(em.find(Ingrediente.class, 9L));
+		ir9.setQuantidade(200);
+		em.persist(ir9);
+		
+		Receita_Ingrediente ir10 = new Receita_Ingrediente();
+		ir10.setReceita(em.find(Receita.class, 1L));
+		ir10.setIngrediente(em.find(Ingrediente.class, 10L));
+		ir10.setQuantidade(200);
+		em.persist(ir10);
+	}
+	
+	public void initIng_R2() {
+		Receita_Ingrediente ir1 = new Receita_Ingrediente();//bolbo
+		ir1.setReceita(em.find(Receita.class, 2L));
+		ir1.setIngrediente(em.find(Ingrediente.class, 11L));
+		ir1.setQuantidade(200);
+		em.persist(ir1);
+		
+		Receita_Ingrediente ir2 = new Receita_Ingrediente();//cenoura
+		ir2.setReceita(em.find(Receita.class, 2L));
+		ir2.setIngrediente(em.find(Ingrediente.class, 12L));
+		ir2.setQuantidade(200);
+		em.persist(ir2);
+		
+		Receita_Ingrediente ir3 = new Receita_Ingrediente();//ovos
+		ir3.setReceita(em.find(Receita.class, 2L));
+		ir3.setIngrediente(em.find(Ingrediente.class, 13L));
+		ir3.setQuantidade(200);
+		em.persist(ir3);
+		
+		Receita_Ingrediente ir4 = new Receita_Ingrediente();//cebolinho
+		ir4.setReceita(em.find(Receita.class, 2L));
+		ir4.setIngrediente(em.find(Ingrediente.class, 14L));
+		ir4.setQuantidade(200);
+		em.persist(ir4);
+		
+		Receita_Ingrediente ir5 = new Receita_Ingrediente();//acucar
+		ir5.setReceita(em.find(Receita.class, 2L));
+		ir5.setIngrediente(em.find(Ingrediente.class, 15L));
+		ir5.setQuantidade(200);
+		em.persist(ir5);
+		
+		Receita_Ingrediente ir6 = new Receita_Ingrediente();//cebola
+		ir6.setReceita(em.find(Receita.class, 2L));
+		ir6.setIngrediente(em.find(Ingrediente.class, 3L));
+		ir6.setQuantidade(200);
+		em.persist(ir6);
+		
+		Receita_Ingrediente ir7 = new Receita_Ingrediente();//azeite
+		ir7.setReceita(em.find(Receita.class, 2L));
+		ir7.setIngrediente(em.find(Ingrediente.class, 6L));
+		ir7.setQuantidade(200);
+		em.persist(ir7);
+	
+		Receita_Ingrediente ir8 = new Receita_Ingrediente();//tomates fritos
+		ir8.setReceita(em.find(Receita.class, 2L));
+		ir8.setIngrediente(em.find(Ingrediente.class, 7L));
+		ir8.setQuantidade(200);
+		em.persist(ir8);
+				
+	}
+	
+	public void initIng_R3() {
+		Receita_Ingrediente ir1 = new Receita_Ingrediente(); //borrego pa
+		ir1.setReceita(em.find(Receita.class, 3L));
+		ir1.setIngrediente(em.find(Ingrediente.class, 16L));
+		ir1.setQuantidade(200);
+		em.persist(ir1);
+		
+		Receita_Ingrediente ir2 = new Receita_Ingrediente(); //vinagre sidra
+		ir2.setReceita(em.find(Receita.class, 3L));
+		ir2.setIngrediente(em.find(Ingrediente.class, 17L));
+		ir2.setQuantidade(200);
+		em.persist(ir2);
+		
+		Receita_Ingrediente ir3 = new Receita_Ingrediente(); //batata assada
+		ir3.setReceita(em.find(Receita.class, 3L));
+		ir3.setIngrediente(em.find(Ingrediente.class, 18L));
+		ir3.setQuantidade(200);
+		em.persist(ir3);
+		
+		Receita_Ingrediente ir4 = new Receita_Ingrediente(); //alho fances
+		ir4.setReceita(em.find(Receita.class, 3L));
+		ir4.setIngrediente(em.find(Ingrediente.class, 19L));
+		ir4.setQuantidade(200);
+		em.persist(ir4);
+		
+		Receita_Ingrediente ir5 = new Receita_Ingrediente(); //maca reinata
+		ir5.setReceita(em.find(Receita.class, 3L));
+		ir5.setIngrediente(em.find(Ingrediente.class, 20L));
+		ir5.setQuantidade(200);
+		em.persist(ir5);
+		
+		Receita_Ingrediente ir6 = new Receita_Ingrediente(); //coentro
+		ir6.setReceita(em.find(Receita.class, 3L));
+		ir6.setIngrediente(em.find(Ingrediente.class, 21L));
+		ir6.setQuantidade(200);
+		em.persist(ir6);
+	}
 }
