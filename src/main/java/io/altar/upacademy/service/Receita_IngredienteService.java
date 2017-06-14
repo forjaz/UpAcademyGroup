efdbd17a1bd3afc5e7e4c879b953a13c6ba1ac12
francisco <alunotarde@altars-Mini-8.lan>
2017-06-14 19:33:58 +0100

    Gerador de receitas, quase feito

--- src/main/java/io/altar/upacademy/service/Receita_IngredienteService.java
+++ src/main/java/io/altar/upacademy/service/Receita_IngredienteService.java
@@ -1,9 +1,11 @@
 package io.altar.upacademy.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
@@ -18,6 +20,17 @@ import io.altar.upacademy.model.Receita_Ingrediente;
 public class Receita_IngredienteService extends EntityService implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	private megaRepository megaBean;
+	
+	public megaRepository getMegaBean() {
+		return megaBean;
+	}
+
+	public void setMegaBean(megaRepository megaBean) {
+		this.megaBean = megaBean;
+	}
+
 	// Constructor
 	public Receita_IngredienteService() {
 		
@@ -39,15 +52,30 @@ public class Receita_IngredienteService extends EntityService implements Seriali
 		return lista;
 	}
 	
-	public List<Receita_Ingrediente> returnIdReceitaIngrediente(){
-		List<Receita_Ingrediente> lista = em.createQuery("SELECT e.Id_Receita FROM Receita_Ingrediente e WHERE e.id=1").getResultList();
+	public List<Receita_Ingrediente> returnReceitaIngrediente(){
+		List<Receita_Ingrediente> lista = em.createQuery("SELECT e FROM Ingrediente e").getResultList();
 		return lista;
 	}
+	
+	// GERADOR DE RECEITAS
+	public List<Ingrediente> returnIdReceitaIngrediente(){
+		String var = megaBean.getProcura();
+		List<Ingrediente> lista = em.createQuery("SELECT id FROM Ingrediente e WHERE nome='"+var+"'").getResultList();
+		return lista;
+	}
+	
+//	public List<Receita_Ingrediente> idReceita(){
+//		String a = returnIdReceitaIngrediente().toString();
+//		List<Receita_Ingrediente> lista = em.createQuery("SELECT receita.id FROM Receita_Ingrediente e WHERE ingrediente.id='"+a+"'").getResultList();
+//		return lista;
+//	}
 	
 	// Getters and Setters
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
+
 	
 	
 	
